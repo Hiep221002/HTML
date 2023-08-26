80ef3be97801c75b44367d2f8e4a7ba84acbec12
Hiep221002 <trandaihiep02@gmail.com>
2023-08-26 21:24:11 +0700

    bien so

--- object/c#/Program.cs
+++ object/c#/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[]args)
+    {
+     int a =4;
+         Console.WriteLine("int :" +a);
+    long b =8;
+        Console.WriteLine("Long:" +b);
+    float c =4;
+        Console.WriteLine("float:" +c);
+    double d =8;
+        Console.WriteLine("Double :" +d);
+    decimal e =16;
+        Console.WriteLine("decimal :" + e);
+    bool f =true;
+        Console.WriteLine("bool :" +f);
+    char g ='j';
+        Console.WriteLine("char :" +g);
+    string i ="luu giu chuoi ki tu bao quoanh boi dau ngoac kep";
+        Console.WriteLine("String :" +i);
+    }
+}
