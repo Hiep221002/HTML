35f3cfb9c6c9119ead918fd774d0826d6e8d223b
Hiep221002 <trandaihiep02@gmail.com>
2023-08-27 22:05:18 +0700

    cac toan tu

--- object/c#/Program.cs
+++ object/c#/Program.cs
@@ -2,21 +2,11 @@
 {
     private static void Main(string[]args)
     {
-     const int a =4;
-         Console.WriteLine("int :" +a);
-    const long b =8;
-        Console.WriteLine("Long:" +b);
-    const float c =4;
-        Console.WriteLine("float:" +c);
-    const double d =8;
-        Console.WriteLine("Double :" +d);
-    const decimal e =16;
-        Console.WriteLine("decimal :" + e);
-    const bool f =true;
-        Console.WriteLine("bool :" +f);
-    const char g ='j';
-        Console.WriteLine("char :" +g);
-    const string i ="luu giu chuoi ki tu bao quoanh  dau ngoac kep";
-        Console.WriteLine("String :" +i);
+        int a,b;
+        System.Console.Write("a=");
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("b=");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0} - * / {1} = {2}",a,b,a - * /b);
     }
 }
